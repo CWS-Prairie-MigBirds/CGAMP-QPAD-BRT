57554f3ae7a17157b7af2c3b5924b4918d07c1a4
Barry Robinson <barry.robinson@ec.gc.ca>
2025-08-06 10:40:14 -0600

    Created script to generate year rasters for model extrapolation

---
+++ Scripts/3_SpatialCovariates/Script7_CovariatePrep_Year.R
@@ -0,0 +1,21 @@
+#Create a covariate raster for year (YYYY) to be used for model extrapolation
+
+library(terra)
+
+#load template raster
+template <- rast("Data/SpatialCovariates/cgamp_ras.tif")
+
+#populate with years of interest and save
+#define years of interest
+years <- c(2020)
+YYYY.list <- lapply(years, FUN = function(x) {
+  yyyy.tmp = template
+  yyyy.tmp[!is.na(yyyy.tmp)] = x
+  names(yyyy.tmp) = paste0("YYYY",x)
+  return(yyyy.tmp)
+})
+
+lapply(YYYY.list, FUN = function(x) {
+  writeRaster(x, paste0("Data/SpatialCovariates/YYYY/",names(x), ".tif"))
+})
+
