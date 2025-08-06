74ed6bde3984d788211a6dbd7e1fd57402baaa5f
Barry Robinson <barry.robinson@ec.gc.ca>
2025-08-06 10:10:01 -0600

    Started to update script to work for multiple species

--- Scripts/4_BRT/Script7a_AssessRelativeImportance.R
+++ Scripts/4_BRT/Script7a_AssessRelativeImportance.R
@@ -1,9 +1,12 @@
 #Investigate relative influence of variables in boosted regression tree models across bootstrap samples
 library(gbm)
 
-#load bootstrap models
-models <- lapply(list.files("C:/Users/RobinsonBa/OneDrive - EC-EC/Documents/Projects/CGAMP/Data/Temp/QingBootstrapModels", full.names = T), readRDS)
+#load bootstrap models and obtain summaries
+models <- list.files("Output/Models_bootstrap", full.names = T)
+models <- lapply(list.files("Output/Models_bootstrap", full.names = T), readRDS)
 rel.inf <- lapply(models, summary)
+
+#split by species
 rel.inf.merge <- Reduce(function(...) merge(..., by = "var"), rel.inf)
 ri.sum <- rowMeans(rel.inf.merge[,2:101])
 ri.sum <-cbind(rel.inf.merge$var,ri.sum)
